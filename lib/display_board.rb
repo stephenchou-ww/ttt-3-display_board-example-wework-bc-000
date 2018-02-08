dd834208139dc48189beef6f1e322b14beba9667
stephenchou-ww <stephen.chou@wework.com>
2018-02-08 00:03:49 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,13 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-def 
+def display_board
+  puts "   |   |   "
+  puts "------------"
+  puts "   |   |   "
+  puts "------------"
+  puts "   |   |   "
+end
+
+display_board
+
 
