16f7427f8cb232c2f7b27515386e8c0bb09a3683
stephenchou-ww <stephen.chou@wework.com>
2018-02-08 00:04:42 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 
 def display_board
   puts "   |   |   "
-  puts "------------"
+  puts "-----------"
   puts "   |   |   "
-  puts "------------"
+  puts "-----------"
   puts "   |   |   "
 end
 
